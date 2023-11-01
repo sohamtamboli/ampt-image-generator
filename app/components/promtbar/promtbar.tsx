7d3a39fd994e8d59b6e9912c50977f2b4e08425a
mohammed khan <mohdkhan.mk99@gmail.com>
2023-11-01 16:07:35 +0530

    input bar changed to text area

--- app/components/promtbar/promtbar.tsx
+++ app/components/promtbar/promtbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 
-const SearchBar: React.FC = () => {
+const Promtbar: React.FC = () => {
   const [query, setQuery] = useState('');
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -14,24 +14,26 @@ const SearchBar: React.FC = () => {
     <div className="ml-72 flex-auto justify-center">
       <form onSubmit={handleSubmit}>
         <div className="relative ">
-          <input
-            type="search"
+        <textarea
+           
             id="default-search"
-            className=" mt-20 flex w-9/12 rounded-3xl border border-blue-500  bg-gray-700 p-5 pl-10 text-base text-white placeholder-gray-400 shadow-lg shadow-gray-200 ring-blue-500 focus:border-blue-500 focus:ring-blue-500 "
+            className=" mt-20 flex w-9/12  rounded-3xl border border-blue-500  bg-gray-700 p-5 pl-10 text-base text-white placeholder-gray-400 shadow-lg shadow-gray-200 ring-blue-500 focus:border-blue-500 focus:ring-blue-500 "
             placeholder="Type your command ,we will generate images for you"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
+          <div className='pt-5  ml-85'>
           <button
             type="submit"
-            className=" absolute bottom-2.5 right-2.5  mr-76 rounded-3xl  bg-blue-700 px-8 py-3 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 "
+            className="  rounded-3xl  bg-blue-700 px-14 py-3  text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 "
           >
             Generate
           </button>
+          </div>
         </div>
       </form>
     </div>
   );
 };
 
-export default SearchBar;
+export default Promtbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       margin: {
         '76': '308px',
+        '85':'350px'
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 };
 export default config;
