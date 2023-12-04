55593faed2eaf1f1d0277ac6c2ff1cd649e8120b
Soham Tamboli <soham.tamboli@antstack.io>
2023-12-04 13:37:14 +0530

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Layout from '../components/layout';
 import SignupForm from '../components/signup/signup';
-Layout;
+
 const SignupPage = () => {
   return (
-    <div>
+    <>
       <SignupForm />
-    </div>
+    </>
   );
 };
 
